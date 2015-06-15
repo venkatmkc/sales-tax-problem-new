9230bea0e1741c20dbf52d7da28759214329798a
Venkatmkc <venkatmkc@gmail.com>
2015-06-15 11:49:07 +0530

    ADded spec, should be able to get user input

---
+++ src/ConsoleInputOutput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ConsoleInputOutput {
+    private Scanner consoleInput;
+
+    public ConsoleInputOutput(Scanner consoleInput) {
+        this.consoleInput = consoleInput;
+    }
+
+    public String getUserInput() {
+        return consoleInput.nextLine();
+    }
+
+    public void displayOutputToUser(Object output) {
+        System.out.println(output);
+    }
+}
